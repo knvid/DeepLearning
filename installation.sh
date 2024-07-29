1b451e9322f4e7174e204ee35cb867d18021545e
knvid <knvidhya@outlook.com>
2024-07-29 17:18:05 -0400

    add textbook materials

--- installation.sh
+++ installation.sh
@@ -1,7 +1,16 @@
-conda create --name d2l python=3.9 -y
-
+# create conda environment
+conda create -n d2l python=3.9 -y
 conda activate d2l
 
+# install tools
 pip install torch==2.0.0 torchvision==0.15.1
-
 pip install d2l==1.0.3
+
+# get textbook materials
+mkdir d2l-en && cd d2l-en
+curl https://d2l.ai/d2l-en-1.0.3.zip -o d2l-en.zip
+unzip d2l-en.zip && rm d2l-en.zip
+cd pytorch
+
+# start jupyter notebook server
+jupyter notebook
